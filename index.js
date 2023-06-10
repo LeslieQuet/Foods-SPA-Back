4a40d7f044662bccece881ee3bd4a3e4795d8260
LeslieQuet <quetglasleslie@gmail.com>
2023-06-09 23:01:45 -0300

    Message error on get rour modified, testing on deploy

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT || 3001, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
